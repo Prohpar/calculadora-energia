5d316f636deacc7ee019c158d1ddf269028fd8d9
Prohpar <arturohernandez.prodimic@gmail.com>
2026-09-18 10:37:37 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ st.markdown(f'''
     background-repeat: no-repeat;
     background-position: center 40%;
     background-size: 65% auto;
-    opacity: 0.80;
+    opacity: 0.30;
     pointer-events: none;
     z-index: 0;
 }}
